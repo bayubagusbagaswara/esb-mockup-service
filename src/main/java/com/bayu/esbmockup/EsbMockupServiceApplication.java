8a79b6ff55fd82787c214e81581391243f335aae
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-09-14 06:57:52 +0700

    Initial commit

---
+++ src/main/java/com/bayu/esbmockup/EsbMockupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.esbmockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbMockupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbMockupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/esbmockup/EsbMockupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.esbmockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbMockupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
